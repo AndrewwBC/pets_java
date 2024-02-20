0e4fa1d773d304580a09eae5e948a4b9f7b0d07c
AndrewwBC <andrew-hot2011@hotmail.com>
2024-02-20 18:52:22 -0300

    begin jwt auth

--- src/main/java/com/example/pets4ever/Controller/User.java
+++ src/main/java/com/example/pets4ever/Controller/User.java
@@ -1,6 +1,24 @@
 package com.example.pets4ever.Controller;
 
 
+import jakarta.servlet.annotation.HttpMethodConstraint;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
 public class User {
+
+    @RequestMapping(value = "/users/andrew", method = RequestMethod.GET)
+    @Validated
+    public String showAndrewName() {
+        return "Andrew";
+    }
+
+
+
+
 
 }
