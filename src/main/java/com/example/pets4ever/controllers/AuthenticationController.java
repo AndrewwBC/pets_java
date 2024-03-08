f8ff6f236326de22013488db132e97954b9d5e85
Andrew <andrew-hot2011@hotmail.com>
2024-03-08 11:21:28 -0300

    add: EmailService

--- src/main/java/com/example/pets4ever/controllers/AuthenticationController.java
+++ src/main/java/com/example/pets4ever/controllers/AuthenticationController.java
@@ -55,11 +55,9 @@ public class AuthenticationController {
         if(this.userRepository.findByName(data.name()) != null) {
             registerErrors.add(new RegisterError("username", "Nome de usuário já cadastrado!"));
         }
-
         if (this.userRepository.findByEmail(data.email()) != null) {
             registerErrors.add(new RegisterError("email", "Email já cadastrado!"));
         };
-
         if(!registerErrors.isEmpty()){
             return ResponseEntity.status(HttpStatus.FORBIDDEN).body(registerErrors);
         }
@@ -72,13 +70,10 @@ public class AuthenticationController {
 
         if(data.password().isEmpty()) {
             User newUser = new User(data.name(), data.email(), data.password(), this.userRole);
-
             this.userRepository.save(newUser);
-
         } else {
             String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
             User newUser = new User(data.name(), data.email(), encryptedPassword, this.userRole);
-
             this.userRepository.save(newUser);
         }
 
