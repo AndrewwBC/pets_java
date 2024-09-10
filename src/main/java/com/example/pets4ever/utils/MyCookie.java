3b31d74a7e74a5fef91ad659436c1b3eb7612f64
AndrewwBC <andrew-hot2011@hotmail.com>
2024-09-10 14:29:11 -0300

    change samesite to none

--- src/main/java/com/example/pets4ever/utils/MyCookie.java
+++ src/main/java/com/example/pets4ever/utils/MyCookie.java
@@ -8,7 +8,7 @@ public class MyCookie {
         ResponseCookie cookie = ResponseCookie.from(name, value)
                 .httpOnly(httpOnly)
                 .secure(true) // Set true for production, HTTPS only
-                .sameSite("Lax")
+                .sameSite("none")
                 .maxAge(age * 60 * 60)
                 .domain("pets4ever.site") // Set for production
                 .path("/")
