5f89995269f592ba787dd03c14aae5686237df72
AndrewwBC <andrew-hot2011@hotmail.com>
2024-09-21 14:32:49 -0300

    removed yml

--- src/main/java/com/example/pets4ever/infra/cors/CorsConfiguration.java
+++ src/main/java/com/example/pets4ever/infra/cors/CorsConfiguration.java
@@ -9,7 +9,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://127.0.0.1:443")
+                .allowedOrigins("https://api.pets4ever.site")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
