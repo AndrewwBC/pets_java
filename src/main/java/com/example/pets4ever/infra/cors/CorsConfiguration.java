38ed48b01a9cdf4c5ec4454c6c171cbe3df55e9e
AndrewwBC <andrew-hot2011@hotmail.com>
2024-11-18 16:24:54 -0300

    add: env of cookies and cors config

--- src/main/java/com/example/pets4ever/infra/cors/CorsConfiguration.java
+++ src/main/java/com/example/pets4ever/infra/cors/CorsConfiguration.java
@@ -1,15 +1,19 @@
 package com.example.pets4ever.infra.cors;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Value("${CORS}")
+    private String CORS;
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://pets4ever.site") // https://pets4ever.site // http://localhost:5173
+                .allowedOrigins(this.CORS) // https://pets4ever.site // http://localhost:5173
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
--- src/main/java/com/example/pets4ever/utils/MyCookie.java
+++ src/main/java/com/example/pets4ever/utils/MyCookie.java
@@ -1,8 +1,12 @@
 package com.example.pets4ever.utils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseCookie;
 
 
 public class MyCookie {
+
+    @Value("${COOKIE}")
+    private String COOKIE;
 
     public ResponseCookie generateCookie(String name, String value, Integer age, boolean httpOnly, boolean secure) {
         ResponseCookie cookie = ResponseCookie.from(name, value)
@@ -10,7 +14,7 @@ public class MyCookie {
                 .secure(true) // Set true for production, HTTPS only
                 .sameSite("Lax")
                 .maxAge(age * 60 * 60)
-                .domain("pets4ever.site") // Set for production pets4ever.site - .localhost
+                .domain(this.COOKIE) // Set for production pets4ever.site - .localhost
                 .path("/")
                 .build();
         return cookie;
